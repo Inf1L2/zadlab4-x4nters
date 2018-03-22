27b980e87650135aa67233b3eb19d611db522cb9
x4nters <x4nters@gmail.com>
2018-03-22 11:33:30 +0100

    Utworzone klasy figur (zad4)

--- POLab4/src/main/java/pl/edu/ur/polab4/Main.java
+++ POLab4/src/main/java/pl/edu/ur/polab4/Main.java
@@ -1,12 +1,29 @@
 package pl.edu.ur.polab4;
-
+import java.util.Scanner;
+// TODO zad 4, 5
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO zad 4, 5
+    private static void zadanie4(){
+        
     }
-    
+
+    private static void zadanie5(){
+
+    }
+
+    public static void main(String[] args) {
+        int menu = 1;
+
+        while (menu != 0){
+            System.out.println("Menu > Wybierz >");
+            System.out.println(" [ 4 ] Zadanie 4");
+            System.out.println(" [ 5 ] Zadanie 5");
+            System.out.print("Menu > ");
+
+            switch (menu){
+                case 4: zadanie4(); break;
+                case 5: zadanie5(); break;
+                case 0: break;
+            }
+        }
+    }
 }
